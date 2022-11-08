ed9e6818bcca58efac96a64339a3b81b929d6330
csabi <csabi@gmail.com>
2022-11-08 20:05:38 +0100

    0

--- lista.py
+++ lista.py
@@ -3,3 +3,9 @@ print(nums[0])              # index /hányadik a listában/ 0-val kezdődik
 
 names = ["Pali", "Dani", "Mari", "Kati"]
 
+flowers = []
+flowers.append("gaz")       # az append parancsal fűzünk a listához elemeket
+flowers.append("paraj")
+print(flowers)
+
+print(len(flowers))         # len = a lista elemeinek a számát adja vissza
