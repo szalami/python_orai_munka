e05e8f6d676935aa8cb07fad304369ad736c35fe
csabi <csabi@gmail.com>
2022-11-08 19:53:58 +0100

    ciklus+

---
+++ bekeres_ciklusra.py
@@ -0,0 +1,19 @@
+sum = 0
+num = -1
+
+while num != 0:
+    num = int(input("szám: "))
+    sum = sum + num
+
+print(sum)
+
+
+sum = 1
+num = -1
+
+while num != 0:
+        num = int(input("szám: "))
+        if num != 0:
+            sum = sum * num
+
+print(sum)
--- ciklus.py
+++ ciklus.py
@@ -1,2 +1,14 @@
-for i in range(0,5):
-    print("Csabi", end=" ")
+for i in range(0,5):            # egyesével lépked a range intervallumában
+    print(i, "Csabi", end=" ")  # ha nincs end akkor sort tör
+
+
+print()
+
+for j in range(0,50,5):         # a harmadik szám: hányasával lépkedjen
+    print(j, end=" ")
+
+print()
+
+for k in range(0,50):           # ugyanaz mint a fenti csak bonyolultan :)
+    if k % 5 == 0:
+        print (k, end=" ")
