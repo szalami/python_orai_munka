37eb00e41510dd72a015560af12bc58d42a81216
csabi <csabi@gmail.com>
2022-11-08 20:28:54 +0100

    kiir

--- kiir.py
+++ kiir.py
@@ -1,9 +1,10 @@
 fp = open("adat.txt", "w", encoding="UTF-8")        # egy fájlt olvasok vele
-                                                    # write w - írásra
+                                                    # write w - írásra, ha nincs ilyen fájl akkor létrehozza, ha már van akkor felülírja
+                                                    
                                                     # read r - olvasásra
                                                     # append a -hozzáfüzésre
 
-fp.write("alma")            # írok a fájlba                    
-fp.write("körte")           # írok a fájlba
+fp.write("alma" + "\n")            # írok a fájlba                    
+fp.write("körte" + "\n")           # írok a fájlba
 
 fp.close                    # ha kész le kell zárni
