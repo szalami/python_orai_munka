fa217dbef76b04fa69ee17db16b0f2bd4e57cb2e
csabi <csabi@gmail.com>
2022-11-08 20:21:09 +0100

    f

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -1,4 +1,14 @@
-def nevjegy():
+def duplaz(szam):           # ide kerül a paraméter (most pl a 6) 
+    eredmeny = szam * 2
+    return eredmeny
+
+result = duplaz(6)          # a 6-ot /paramétert átadom a függvénynek, ami müveletet végez vele
+print(result)               # és visszaadja a művelet eredményét amit eltárolok egy változóban
+print(duplaz(7))            # vagy kiirathatom egyből
+
+
+
+def nevjegy():              # a függvény törzsében levő össze utasítás végrehajtódik
     print('--------------')
     print('Lutz Gizi')
     print('SZOFT_I_1_E')
