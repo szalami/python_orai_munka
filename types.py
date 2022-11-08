35b06aa3ba0a7bce46b75c9b23948d4a6d65c238
csabi <csabi@gmail.com>
2022-11-08 18:01:01 +0100

    típusok

---
+++ types.py
@@ -0,0 +1,33 @@
+print("alma")   #string
+print(45)       #egesz
+print(45.0)     #valós
+print(True)     #boolean /igaz - hamis/
+print(False)    #boolean /igaz - hamis/
+
+# változók
+
+szam = 45
+print("A szam változó:", szam)
+szam = 65
+print("A megváltozott szam változó:", szam)
+
+print(type(szam))       # int   = egész
+print(type(45.2))       # float = lebegőpontos
+print(type(0.452E+2))    # float = lebegőpontos /tudományos alak/
+print(type("alma"))     # str   = szöveg
+print(type(True))       # bool  = boolean
+
+szam = 45
+eredmeny = szam + 3     #kifejezés = 1 vagy több változó vagy állandó és 1 operáto /műveleti jel/
+                        # az a = jel -> értékadó operátor 
+szo1 = "alma"
+szo2 = "körte"
+mondat = szo1 + " " + szo2  
+print(mondat)
+
+szam2 = float(5)    # megváltoztatom a típusát a float függvénnyel
+print(type(szam2))
+
+
+
+
